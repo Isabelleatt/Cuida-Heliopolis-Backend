6829ae82a9d7421a824cb2a306d6a090dfac5875
EduardoMVAz <emendesvaz@gmail.com>
2023-05-31 08:42:31 -0300

    changes

---
+++ cuida_heliopolis/src/main/java/com/insper/cuida_heliopolis/usuario/Responsavel.java
@@ -0,0 +1,14 @@
+package com.insper.cuida_heliopolis.usuario;
+
+import jakarta.persistence.Entity;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+public class Responsavel {
+    
+}
