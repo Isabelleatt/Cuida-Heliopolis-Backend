9bcd62c510d374a93a26154af66affa487c47e8a
EduardoMVAz <emendesvaz@gmail.com>
2023-06-02 15:03:17 -0300

    Relações avaliação/usuário

--- cuida_heliopolis/src/main/java/com/insper/cuida_heliopolis/avaliacao/AvaliaResponsavel.java
+++ cuida_heliopolis/src/main/java/com/insper/cuida_heliopolis/avaliacao/AvaliaResponsavel.java
@@ -1,5 +1,7 @@
 package com.insper.cuida_heliopolis.avaliacao;
 
+import com.insper.cuida_heliopolis.usuario.Responsavel;
+
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -15,4 +17,8 @@ public class AvaliaResponsavel extends Avaliacao {
     private Integer comportamento;
     private Integer pontualidade;
     private Integer pagamento;
+
+    @ManyToOne
+    @JoinColumn(name = "responsavel_id")
+    private Responsavel responsavel;
 }
--- cuida_heliopolis/src/main/java/com/insper/cuida_heliopolis/usuario/Responsavel.java
+++ cuida_heliopolis/src/main/java/com/insper/cuida_heliopolis/usuario/Responsavel.java
@@ -3,9 +3,11 @@ package com.insper.cuida_heliopolis.usuario;
 import java.util.List;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.insper.cuida_heliopolis.avaliacao.AvaliaResponsavel;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.ManyToMany;
+import jakarta.persistence.OneToMany;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -20,4 +22,6 @@ public class Responsavel extends Usuario{
     @ManyToMany(mappedBy = "responsaveis")
     private List<Cuidador> cuidadores;
 
+    @OneToMany(mappedBy = "responsavel")
+    private List<AvaliaResponsavel> avaliacoes;
 }
